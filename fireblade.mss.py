867dc2d3bbf5ae79f4bffa9021bec3e28735a044
Adam Chu <adamchu@sfu.ca>
2023-05-20 18:37:44 -0700

    v0.7

--- fireblade.mss.py
+++ fireblade.mss.py
@@ -5,7 +5,7 @@ from jnpr.junos import Device
 from jnpr.junos.exception import *
 from jnpr.junos.utils.start_shell import StartShell
 from jnpr.junos.utils.config import Config
-from utils import formatter
+from utils import formatter, fireblade_hw
 import concurrent.futures
 
 # get and process command line options
@@ -27,9 +27,17 @@ def getArgs():
     parser.add_argument('-m', '--mode', choices=['show','testconfig', 'commit'], default='show', 
         help='Operation mode: Default to "show", options of "testconfig" and "commit"')
 
-# legacy option 'port' for NETCONF
-#    parser.add_argument('-p', '--port', choices = ['830', '80'], default = '830', 
-#        help='TCP port for NETCONF session. 830 by default otherwise 80')
+    # option 'model'
+    parser.add_argument('-d', '--model', choices=['g', 'p', 'mp'], default='general',
+        help='Chassis model: This option is only needed when operation mode is "testconfig" or "commit". ' + 
+        'Default to "g" for "general" when proposing changes are irrelevant to chassis model,' +
+        'other choices are "p" for "EX4300-48P" and "mp" for "EX4300-48MP"')
+
+    # option 'role'
+    parser.add_argument('-r', '--role', choices=['core', 'edge', 'dc', 'ext', 'mgmt'], default='edge',
+        help='Chassis role: This option is only needed when operation mode is "testconfig" or "commit". ' + 
+        'Default to "edge" for regular edge switches. Other choices are "core", "ext" for extension switches, ' + 
+        '"dc" for data centre switches, and "mgmt" for management switches.')
 
     # take available options
     args = parser.parse_args()
@@ -54,7 +62,7 @@ def getArgs():
         with open(f"{args.cmdfile}", "r") as fo:
             commands = [line.strip() for line in fo.readlines() if not line.startswith('#')]
 
-    return hosts, commands, args.mode#, args.port
+    return hosts, commands, args.mode, args.model, args.role #, args.port
 
 # process credential
 def getCredential():
@@ -66,9 +74,11 @@ def getCredential():
     return credential
 
 # netconf session
-def ncsession(host, commands, mode, uname, passwd):
+def ncsession(host, model, role, commands, mode, uname, passwd):
+
     try:
         with Device(host=host, user=uname, password=passwd) as dev:
+
             print_out = f"\033[1;34m------------------------------------------------\033[0m\nHost: {host}\n"
             
             # mode dictates
@@ -91,6 +101,24 @@ def ncsession(host, commands, mode, uname, passwd):
                 print (print_out)
 
             else:
+
+                # skip change on mis-matched role
+                if fireblade_hw.role(dev) != role:
+                    print_out += f"\nThis host is not a '{role}'' switch, skipping changes."
+                    print (print_out)
+                    return
+
+                # skip change on mis-matched model
+                if model == 'EX4300-48MP' and fireblade_hw.model(dev) != model:
+                    print_out += "\nThis host is not in the same chassis model these changes are proposed to apply on, skipping changes."
+                    print (print_out)
+                    return
+                if model == 'EX4300-48P' and fireblade_hw.model(dev) == 'EX4300-48MP':
+                    print_out += "\nThis host is not in the same chassis model these changes are proposed to apply on, skipping changes."
+                    print (print_out)
+                    return
+
+                # continue on matched chassis
                 with Config(dev, mode='exclusive') as cu:
 
                     # excute commands
@@ -98,7 +126,7 @@ def ncsession(host, commands, mode, uname, passwd):
                         cu.load(command, format='set', ignore_warning=True)
 
                     if cu.diff() != None:
-                        print_out += f'{host} {cu.diff()}\n'
+                        print_out += f'{cu.diff()}\n'
                         try:
                             cu.commit_check(timeout=600)
                             print_out += '\033[32m' + 'Changes passed commit check.' + '\033[0m\n'
@@ -136,6 +164,8 @@ def main():
         hosts = args[0]
         commands = args[1]
         mode = args[2]
+        model = args[3]
+        role = args[4]
     except argparse.ArgumentError as err:
         print(f"Error: {err}")
         return
@@ -146,9 +176,11 @@ def main():
     passwd = credential[1]
     print (commands)
 
+    chassis_model = 'general' if model == 'g' else 'EX4300-48P' if model == 'p' else 'EX4300-48MP' if model == 'mp' else None
+
     # run commands on each host in parallel
     with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
-        futures = [executor.submit(ncsession, host, commands, mode, uname, passwd) 
+        futures = [executor.submit(ncsession, host, chassis_model, role, commands, mode, uname, passwd) 
         for host in hosts]
         concurrent.futures.wait(futures)
 
